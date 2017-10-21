f6ad91c86e9849da25ade5dbc1d97232f0dcffc9
Vladimir Kivernik <vladimir@kivernik.ru>
2017-10-21 17:14:02 +0500

    Added post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Blog Posts
+    """
+    title = models.CharField(max_length=256)
+    slug = models.SlugField()
+    body = models.TextField()
+    created_date = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'запись'
+        verbose_name_plural = 'записи'
