b61b1edc45be4ee729feb2c9f52cca3a3cb6d1e3
Vladimir Kivernik <vladimir@kivernik.ru>
2017-10-21 17:32:34 +0500

    Added language, tz, verbose_name to Post model fields

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ['title', 'slug', 'body']
+
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,10 +5,10 @@ class Post(models.Model):
     """
     Blog Posts
     """
-    title = models.CharField(max_length=256)
-    slug = models.SlugField()
-    body = models.TextField()
-    created_date = models.DateTimeField(auto_created=True)
+    title = models.CharField(max_length=256, verbose_name='заголовок')
+    slug = models.SlugField(verbose_name='Url')
+    body = models.TextField(verbose_name='заметка')
+    created_date = models.DateTimeField(auto_created=True, verbose_name='дата создания')
 
     def __str__(self):
         return self.title
